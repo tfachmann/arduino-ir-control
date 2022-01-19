d05af0df21dc7d4db9ce5933fe67cd75d2da1b1b
tfachmann <fachmanntj@gmail.com>
2022-01-19 21:50:50 +0100

    build: add build files for arduino

---
+++ BoardOptions.cmake
@@ -0,0 +1,37 @@
+# Copyright (c) 2020 Arduino CMake Toolchain
+
+###############################################################################
+# This is an automatically generated template file for board options.
+# You may edit it to comment/uncomment selected board and board options.
+# However do not change the structure of this template, which is fixed and 
+# any change to the structure gets overwritten.
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Arduino boards.
+# Platform: Arduino AVR Boards
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+set(ARDUINO_BOARD "Arduino Uno [avr.uno]") # Arduino Uno
+
+# Option: Processor
+set(ARDUINO_AVR_DIECIMILA_MENU_CPU_ATMEGA328 TRUE) # ATmega328P
+
+# Option: Processor
+set(ARDUINO_AVR_NANO_MENU_CPU_ATMEGA328 TRUE) # ATmega328P
+
+# Option: Processor
+set(ARDUINO_AVR_MEGA_MENU_CPU_ATMEGA2560 TRUE) # ATmega2560 (Mega 2560)
+
+# Option: Processor
+set(ARDUINO_AVR_MINI_MENU_CPU_ATMEGA328 TRUE) # ATmega328P
+
+# Option: Processor
+set(ARDUINO_AVR_BT_MENU_CPU_ATMEGA328 TRUE) # ATmega328P
+
+# Option: Processor
+set(ARDUINO_AVR_LILYPAD_MENU_CPU_ATMEGA328 TRUE) # ATmega328P
+
+# Option: Processor
+set(ARDUINO_AVR_PRO_MENU_CPU_16MHZATMEGA328 TRUE) # ATmega328P (5V, 16 MHz)
+
+# Option: Processor
+set(ARDUINO_AVR_ATMEGANG_MENU_CPU_ATMEGA168 TRUE) # ATmega168

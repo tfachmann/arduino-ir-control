35f106c7896ddf2b94c49caa0870533bb1502ff4
tfachmann <fachmanntj@gmail.com>
2022-01-30 11:55:33 +0100

    feat: refactor to be a class, supports systemd

--- scripts/watcher.py
+++ scripts/watcher.py
@@ -1,54 +1,96 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import serial
-import time
-import sys
+import argparse
+import logging
 import subprocess
+import sys
+import time
+
+import serial
 
 bytes_dict = {
-    b'F00FFF00': "up",
-    b'F609FF00': "up",
-    b'E31CFF00': "down",
-    b'E21DFF00': "down",
-    b'E817FF00': 10,
-    b'ED12FF00': 20,
-    b'E916FF00': 30,
-    b'BF40FF00': 40,
-    b'B34CFF00': 50,
-    b'FB04FF00': 60,
-    b'F50AFF00': 70,
-    b'E11EFF00': 80,
-    b'F10EFF00': 90,
-    b'EB14FF00': 100,
+    b"F00FFF00": "up",
+    b"F609FF00": "up",
+    b"E31CFF00": "down",
+    b"E21DFF00": "down",
+    b"E817FF00": 10,
+    b"ED12FF00": 20,
+    b"E916FF00": 30,
+    b"BF40FF00": 40,
+    b"B34CFF00": 50,
+    b"FB04FF00": 60,
+    b"F50AFF00": 70,
+    b"E11EFF00": 80,
+    b"F10EFF00": 90,
+    b"EB14FF00": 100,
 }
 delta = 1
 
+log = logging.getLogger("watcher.py")
+log.setLevel(logging.INFO)
 
-def main():
-    ser = serial.Serial("/dev/ttyACM0", 9600)
 
-    while True:
-        data = ser.readline()
+class SerialWatcher:
+    def __init__(self, device="/dev/ttyACM0", baud=9600) -> None:
+        self.wait = 10
+        self.device = device
+        self.baud = baud
+        self.cur_brightness = 30
+        self.init_serial()
 
-        for known_bytes in bytes_dict.keys():
-            if not known_bytes in data:
+    def init_serial(self):
+        while True:
+            try:
+                self.ser = serial.Serial(self.device, self.baud)
+                log.info(f"Successfully initialized Serial device {self.device} with {self.baud} baud")
+                return
+
+            except serial.SerialException:
+                log.info(f"Could not allocate Serial port, retrying in {self.wait} seconds...")
+                time.sleep(self.wait)
                 continue
-            decoded = bytes_dict[known_bytes]
-            print(decoded)
-            if type(decoded) == int:
-                subprocess.call(["brightness_monitor_external", str(decoded)])
-                cur_brightness = decoded
-            elif decoded == "up":
-                cur_brightness += delta
-                subprocess.call(["brightness_monitor_external", str(cur_brightness)])
-            elif decoded == "down":
-                cur_brightness -= delta
-                subprocess.call(["brightness_monitor_external", str(cur_brightness)])
-        else:
-            print("---")
-            print(data)
+
+    def run(self):
+        while True:
+            data = self.ser.readline()
+
+            for known_bytes in bytes_dict.keys():
+                if not known_bytes in data:
+                    continue
+                decoded = bytes_dict[known_bytes]
+                log.info(f"Received: {data} => {decoded}")
+                if type(decoded) == int:
+                    subprocess.call(["brightness_monitor_external", str(decoded)])
+                    self.cur_brightness = decoded
+                elif decoded == "up":
+                    self.cur_brightness += delta
+                    subprocess.call(["brightness_monitor_external", str(self.cur_brightness)])
+                elif decoded == "down":
+                    self.cur_brightness -= delta
+                    subprocess.call(["brightness_monitor_external", str(self.cur_brightness)])
+            else:
+                log.debug(f"Received unknown: {data}")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Serial port watcher")
+    parser.add_argument("--log", dest="loglevel", default="INFO", type=str)
+    parser.add_argument("--systemd", action="store_true")
+    args = parser.parse_args()
+
+    log.setLevel(getattr(logging, args.loglevel.upper()))
+
+    if args.systemd:
+        import systemd.journal
+
+        handler = systemd.journal.JournalHandler()
+        handler.setFormatter(logging.Formatter("[%(levelname)s]: %(message)s"))
+        log.addHandler(systemd.journal.JournalHandler())
+    else:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+        log.addHandler(handler)
+
+    watcher = SerialWatcher()
+    watcher.run()
